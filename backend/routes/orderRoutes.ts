d9126f961dd5ce4e35060ee9a48fba68bbe3ca7a
alibomba <wojci.bro@gmail.com>
2023-09-22 15:51:08 +0200

    my orders, reports, rwd

--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -10,20 +10,44 @@ const PER_PAGE: number = 6;
 orderRoutes.get('/my-orders', jwtAuthentication, async (req: Request, res: Response) => {
     const { user } = req.body;
     let page = req.query.page as string | number;
-    if(page) page = +page;
+    if (page) page = +page;
     else page = 1;
-    const orderCount = (await prisma.order.findMany({where: {user_id: user.id}})).length;
-    if(orderCount === 0) return res.status(404).json({message: 'Brak zamówień'});
+    const orderCount = (await prisma.order.findMany({ where: { user_id: user.id } })).length;
+    if (orderCount === 0) return res.status(404).json({ message: 'Brak zamówień' });
     const lastPage = Math.ceil(orderCount / PER_PAGE);
     if (page > lastPage) return res.status(404).json({ message: `There are only ${lastPage} pages` });
     const offset = (page - 1) * PER_PAGE;
-    const orders = await prisma.order.findMany({ where: { user_id: user.id }, include: {product: {select: {name: true, images: {where: {is_thumbnail: true}}}}}, take: PER_PAGE, skip: offset });
+    const orders = await prisma.order.findMany({ where: { user_id: user.id }, include: { product: { select: { name: true, images: { where: { is_thumbnail: true } } } } }, take: PER_PAGE, skip: offset });
     const response: PaginationResponse<Order> = {
         currentPage: page,
         lastPage,
         data: orders
     };
     res.json(response);
+});
+
+orderRoutes.post('/report-order', jwtAuthentication, async (req: Request, res: Response) => {
+    const { user, subject, content, orderId } = req.body;
+    if (!orderId) return res.status(422).json({ message: 'Identyfikator zamówienia jest wymagany' });
+    if (!await prisma.order.findUnique({ where: { id: orderId } })) return res.status(422).json({ message: 'Zamówienie nie istnieje' });
+    if (!subject) return res.status(422).json({ message: 'Temat jest wymagany' });
+    if (subject.length > 100) return res.status(422).json({ message: 'Temat może mieć maksymalnie 100 znaków' });
+    if (!content) return res.status(422).json({ message: 'Treść jest wymagana' });
+    if (content.length > 700) return res.status(422).json({ message: 'Treść może mieć maksymalnie 700 znaków' });
+
+    try {
+        await prisma.orderReport.create({
+            data: {
+                subject,
+                content,
+                order_id: orderId,
+                user_id: user.id
+            }
+        });
+        res.status(201).json({ message: 'Wysłano zgłoszenie' });
+    } catch (err) {
+        res.sendStatus(500);
+    }
 });
 
 export default orderRoutes;
--- frontend/src/sections/kontoMyOrders/KontoMyOrders.tsx
+++ frontend/src/sections/kontoMyOrders/KontoMyOrders.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { AiFillWarning } from 'react-icons/ai';
 import { FaArrowRotateLeft } from 'react-icons/fa6';
-import {BsArrowLeft, BsArrowRight} from 'react-icons/bs';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import styles from './kontoMyOrders.module.css';
 import axios from 'axios';
 import axiosClient from '../../axiosClient';
+import Popup from '../../components/popup/Popup';
 
 interface Props {
   setError: React.Dispatch<React.SetStateAction<string | null>>;
@@ -15,6 +16,9 @@ const KontoMyOrders = ({ setError }: Props) => {
   const [orders, setOrders] = useState<PaginationResponse<OrderWithProduct> | null>(null);
   const [noResults, setNoResults] = useState<boolean>(false);
   const [page, setPage] = useState<number>(1);
+  const [reportedId, setReportedId] = useState<string | null>(null);
+  const [reportValidationError, setReportValidationError] = useState<string | null>(null);
+  const [popup, setPopup] = useState<string | null>(null);
 
   useEffect(() => {
     const source = axios.CancelToken.source();
@@ -29,10 +33,10 @@ const KontoMyOrders = ({ setError }: Props) => {
         setOrders(res.data);
       })
       .catch(err => {
-        if(err?.response?.status === 404 && err?.response?.data?.message === 'Brak wyników'){
+        if (err?.response?.status === 404 && err?.response?.data?.message === 'Brak wyników') {
           setNoResults(true);
         }
-        else{
+        else {
           setError('Coś poszło nie tak, spróbuj ponownie później...');
         }
       });
@@ -43,24 +47,54 @@ const KontoMyOrders = ({ setError }: Props) => {
 
   }, [page]);
 
-  function prevPage(){
-    if(orders?.currentPage !== 1){
+  function prevPage() {
+    if (orders?.currentPage !== 1) {
       setPage(prev => prev - 1);
     }
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
-  });
+    });
   }
 
-  function nextPage(){
-    if(orders?.currentPage !== orders?.lastPage){
+  function nextPage() {
+    if (orders?.currentPage !== orders?.lastPage) {
       setPage(prev => prev + 1);
     }
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
-  });
+    });
+  }
+
+  async function sendReport(e: React.FormEvent): Promise<void> {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const subject = form.querySelector('input') as HTMLInputElement;
+    const content = form.querySelector('textarea') as HTMLTextAreaElement;
+
+    try {
+      await axiosClient({
+        method: 'post',
+        url: '/report-order',
+        data: {
+          subject: subject.value,
+          content: content.value,
+          orderId: reportedId
+        }
+      });
+      form.reset();
+      setReportValidationError(null);
+      setReportedId(null);
+      setPopup('Wysłano zgłoszenie');
+      setTimeout(() => setPopup(null), 4000);
+    } catch (err: any) {
+      if (err?.response?.status === 422) {
+        setReportValidationError(err?.response?.data?.message);
+      } else {
+        setError('Coś poszło nie tak, spróbuj ponownie później...');
+      }
+    }
   }
 
   return (
@@ -78,9 +112,9 @@ const KontoMyOrders = ({ setError }: Props) => {
                 <div className={styles.order__data}>
                   <h3 className={styles.order__title}>{order.product.name}</h3>
                   <p className={styles.order__quantity}>{order.quantity}x{order.sold_at_price}zł</p>
-                  <p className={styles.order__price}>{order.quantity*order.sold_at_price}zł</p>
+                  <p className={styles.order__price}>{order.quantity * order.sold_at_price}zł</p>
                 </div>
-                <button className={styles.order__button}>
+                <button onClick={() => setReportedId(order.id)} className={styles.order__button}>
                   <AiFillWarning className={styles.order__icon} />
                   <span className={styles.order__button__text}>Zgłoś problem z zakupem</span>
                 </button>
@@ -109,6 +143,21 @@ const KontoMyOrders = ({ setError }: Props) => {
           }
         </>
       }
+      {
+        reportedId &&
+        <>
+          <form onSubmit={sendReport} className={styles.report}>
+            <input required max={100} aria-label='Temat' placeholder='Temat' className={styles.report__input} type="text" />
+            <textarea required maxLength={700} aria-label='Treść' placeholder='Treść' className={styles.report__textarea} cols={30} rows={10}></textarea>
+            {
+              reportValidationError && <p role='alert' aria-live='assertive' className={styles.report__error}>{reportValidationError}</p>
+            }
+            <button className={styles.report__button}>Wyślij</button>
+          </form>
+          <div onClick={() => { setReportedId(null); setReportValidationError(null); }} className={styles.reportOverlay}></div>
+        </>
+      }
+      <Popup active={popup ? true : false} type='good'>{popup}</Popup>
     </main>
   )
 }
